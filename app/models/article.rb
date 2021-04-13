892d0791fb28958dea95052fe3cf0d064721580a
Nihal Mohammed <mnihal64@outlook.com>
2021-04-13 19:03:48 +0530

    Set up validations

--- app/models/article.rb
+++ app/models/article.rb
@@ -1,2 +1,7 @@
 class Article < ApplicationRecord
+    validates :title, :text, :image, presence: true
+
+    belongs_to :author, class_name: 'User', foreign_key: 'user_id'
+    has_many :votes
+    belongs_to :categories
 end
