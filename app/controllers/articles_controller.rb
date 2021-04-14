26ab2dcce2044ffccd1304a33cacc258dee7bcbf
Nihal Mohammed <mnihal64@outlook.com>
2021-04-14 19:13:54 +0530

    Define article controller methods

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,9 +1,32 @@
 class ArticlesController < ApplicationController
-  def index; end
+  before_action :authenticate_user!
 
-  def show; end
+  def index
+    @articles = Article.all
+  end
 
-  def new; end
+  def show
+    @article = Article.find(params[:id])
+  end
 
-  def create; end
+  def new
+    @article = Article.new
+    @cat = Category.all
+  end
+
+  def create
+    @article = current_user.articles.build(params_article)
+    if @article.save
+      redirect_to root_path
+      flash[:notice] = 'Your article has been submitted!'
+    else
+      render :new
+    end
+  end
+
+  private
+
+  def params_article
+    params.require(:article).permit(:title, :text, :image, :category_id)
+  end
 end
