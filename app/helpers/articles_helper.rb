e74553e41d27d9bfbf45242224dc78527624bbbf
Nihal Mohammed <mnihal64@outlook.com>
2021-04-19 17:39:43 +0530

    Remove unnecessary comments

--- app/helpers/articles_helper.rb
+++ app/helpers/articles_helper.rb
@@ -3,7 +3,6 @@ module ArticlesHelper
     vote = Vote.find_by(article: article, user: current_user)
     if vote
       content_tag(:div) do
-        # link_to article_vote_path(article), method: :delete,
         link_to fa_icon('thumbs-down', class: 'text-decoration-none link-dark fs-5 mx-2'), article_vote_path(article),
                 method: :delete
       end
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,8 +1,1 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
 categories = Category.create!([{ name: 'Competitions', priority: 1 }, { name: 'Equipment', priority: 2 }, { name: 'Routines', priority: 3 }, { name: 'Diet', priority: 4 }], )
